43567ae55c38eb7c83a05deac743704cc553314d
Abhinav Singh <abhinavsingh@Abhinavs-MacBook-Air.local>
2020-07-14 00:59:53 +0530

    Change Appname and update readme

--- EntityValidatorApp/wsgi.py
+++ EntityValidatorApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for VernacularAssignment project.
+WSGI config for EntityValidatorApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VernacularAssignment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EntityValidatorApp.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VernacularAssignment.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EntityValidatorApp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
